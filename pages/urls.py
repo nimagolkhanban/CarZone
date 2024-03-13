04ddca8765f4428f65b36783cd68a50888b165fb
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-03-13 09:13:16 +0330

    add basic page like about us and contact us and ...

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path("services/", views.services, name='services'),
 ]
