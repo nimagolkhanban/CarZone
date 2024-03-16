755a5788ad68b7db255566d1fae4f4bbd63e6eae
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-03-16 03:53:55 +0330

    add a functionality to admin panel and add a mage column to admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Team
+from django.utils.html import format_html
 # Register your models here.
 
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, obj):
+        return format_html('<img src="{}" width="50" style="border-radius:50px;"/>'.format(obj.photo.url))
+
+    thumbnail.short_description = 'photo'
+    list_display = ("id", "thumbnail", "first_name", "designation", "created_date")
+    list_display_links = ("id", "first_name", "thumbnail")
+    list_filter = ("designation", "created_date")
+
+
+
+
+
+
+admin.site.register(Team,TeamAdmin)
