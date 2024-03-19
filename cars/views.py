8885a6ff28ead736d49344b91beebca2ef92f3ea
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-03-20 02:03:02 +0330

    paginator setting ***** realy important to know

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 
 def cars(request):
     cars = Car.objects.order_by("-created_date")
-    return render(request, "cars/cars.html", {"cars": cars})
+    paginator = Paginator(cars,4)
+    page = request.GET.get("page")
+    page_car = paginator.get_page(page)
+    return render(request, "cars/cars.html", {"cars": page_car})
 
 
 def car_detail(request, id):
